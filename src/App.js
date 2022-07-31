7ffd52d8840244447736ab2060757ddbce3fd6c6
DEEPAK LODHA <sdkosm23@gmail.com>
2022-07-31 15:52:58 +0530

    this is bghelp commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+export default class Demo extends Component {
+  render() {
+    return (
+      <div>
+      <h1 className='heading'>Bhagavad Gita In Practical Life</h1>
+      <p>Re-Design By - <u>SDK Production</u></p>
+      <p><u>Reference</u> - <b>iskcon.org</b></p>
+       <br/>
+       <br/>
+
+    <div className='image'>
+      <img src="/bg.jpg" alt="bg"/>
     </div>
-  );
-}
 
-export default App;
+    </div>
+    )
+  }
+}
